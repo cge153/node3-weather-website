4d47eccc70a6349f2760668c218482e33c857327
adm Neumann <adm-neumannchrist@hq.media-saturn.net>
2020-12-26 10:03:08 +0100

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs');
 const { getCoordinates, getWeather } = require('./utils/geocode');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config.
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -70,23 +71,6 @@ app.get('/weather', (req, res) => {
     });
 });
 
-app.get('/products', (req, res) => {
-    const { search, rating, test } = req.query;
-    if (!search) {
-        return res.send({
-            error: 'You must provide a search term!'
-        });
-    }
-
-    console.log(search);
-    console.log(rating);
-    console.log(test);
-
-    res.send({
-        products: []
-    });
-});
-
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
@@ -104,6 +88,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`);
 });
